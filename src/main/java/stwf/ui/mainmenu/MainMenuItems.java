e367ec40844a0d3c2b46b8a30cd6d7570e8d7a8e
Roncon <roncon03@gmail.com>
2022-03-09 12:04:44 -0500

    Initial commit

---
+++ src/main/java/stwf/ui/mainmenu/MainMenuItems.java
@@ -0,0 +1,43 @@
+package stwf.ui.mainmenu;
+
+import java.util.ArrayList;
+
+import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.evacipated.cardcrawl.modthespire.lib.ByRef;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.characters.CharacterManager;
+import com.megacrit.cardcrawl.screens.mainMenu.MainMenuScreen;
+import com.megacrit.cardcrawl.screens.mainMenu.MenuButton;
+
+import javassist.CtBehavior;
+
+@SpirePatch(
+    clz=MainMenuScreen.class,
+    method="setMainMenuButtons"
+)
+public class MainMenuItems
+{
+    @SpireInsertPatch(
+        locator=Locator.class,
+        localvars={"index"}
+    )
+    public static SpireReturn Insert(Object __obj_instance, @ByRef int[] index)
+    {
+        MainMenuScreen __instance = (MainMenuScreen)__obj_instance;
+        __instance.buttons.add(new MenuButton(NewMenuButtons.COOP, index[0]++));
+        __instance.buttons.add(new MenuButton(NewMenuButtons.SINGLE_PLAYER, index[0]++));
+
+        return SpireReturn.Return();
+    }
+
+    private static class Locator extends SpireInsertLocator
+    {
+        @Override
+        public int[] Locate(CtBehavior ctMethodToPatch) throws Exception
+        {
+            Matcher finalMatcher = new Matcher.MethodCallMatcher(CharacterManager.class, "anySaveFileExists");
+            return LineFinder.findInOrder(ctMethodToPatch, new ArrayList<Matcher>(), finalMatcher);
+        }
+    }
+}
---
+++ src/main/java/stwf/util/RichPresencePatch.java
@@ -0,0 +1,91 @@
+package stwf.util;
+
+import com.codedisaster.steamworks.SteamFriends;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.integrations.steam.SteamIntegration;
+
+import basemod.ReflectionHacks;
+import stwf.SpireTogetherWithFriends;
+
+public class RichPresencePatch {
+
+	public static String[] ord = CardCrawlGame.languagePack.getUIString("NumericOrdinals").TEXT;
+
+    @SpirePatch(clz = SteamIntegration.class, method="setRichPresenceDisplayPlaying", paramtypez={int.class, int.class, String.class})
+    public static class PerFloorRichPresenceAscension 
+	{
+        public static void Replace(SteamIntegration __instance, int floor, int ascension, String character) 
+		{
+        	if (SpireTogetherWithFriends.gameMode == SpireTogetherWithFriends.GameModes.Coop)
+			{
+	        	RichPresencePatch.setRP(String.format(CardCrawlGame.languagePack.getUIString("RichPresence").TEXT[2], character, 2, floor, ascension));
+			}
+	        else
+			{
+	        	RichPresencePatch.setRP(String.format(CardCrawlGame.languagePack.getUIString("RichPresence").TEXT[3], RichPresencePatch.ordinal(1), character, floor, ascension));
+			}
+        }
+    }
+
+    @SpirePatch(clz = SteamIntegration.class, method="setRichPresenceDisplayPlaying", paramtypez={int.class, String.class})
+    public static class PerFloorRichPresence 
+	{
+        public static void Replace(SteamIntegration __instance, int floor, String character) 
+		{
+        	if (SpireTogetherWithFriends.gameMode == SpireTogetherWithFriends.GameModes.Coop)
+			{
+				RichPresencePatch.setRP(String.format(CardCrawlGame.languagePack.getUIString("RichPresence").TEXT[4], character, 2, floor));
+			}
+	        else
+			{
+	        	RichPresencePatch.setRP(String.format(CardCrawlGame.languagePack.getUIString("RichPresence").TEXT[5], RichPresencePatch.ordinal(1), character, floor));
+			}
+        }
+    }
+
+    @SpirePatch(clz = SteamIntegration.class, method="setRichPresenceDisplayInMenu")
+    public static class MainMenuRichPresence 
+	{
+        public static void Replace(SteamIntegration __instance) 
+		{
+	        RichPresencePatch.setRP(CardCrawlGame.languagePack.getUIString("RichPresence").TEXT[6]);
+        }
+    }
+
+    public static void setRP(String value) 
+	{
+    	SteamFriends sf = (SteamFriends)ReflectionHacks.getPrivateStatic(SteamIntegration.class, "steamFriends");
+
+	    if (sf != null) 
+		{
+	    	sf.setRichPresence("status", value);
+	    	sf.setRichPresence("steam_display", "#Status");
+	    }
+    }
+
+	public static String ordinal(int i) 
+	{
+	    String[] suffixes = new String[] { RichPresencePatch.ord[0], RichPresencePatch.ord[1], RichPresencePatch.ord[2], RichPresencePatch.ord[3], RichPresencePatch.ord[4], RichPresencePatch.ord[5], RichPresencePatch.ord[6], RichPresencePatch.ord[7], RichPresencePatch.ord[8], RichPresencePatch.ord[9] };
+	    switch (i % 100) {
+	    case 10:
+	    case 11:
+	    case 12:
+	    case 13:
+	    case 20:
+	    case 30:
+	    case 40:
+	        return i + RichPresencePatch.ord[9];
+	    default:
+		    String ord = i + RichPresencePatch.ord[9];
+			try 
+			{
+				ord = i + suffixes[(i % 10)-1];
+			} 
+			catch (Exception e) {}
+
+	        return ord;
+
+	    }
+	}    
+}
