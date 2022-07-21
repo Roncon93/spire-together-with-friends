558d1f023d8fa6f8cdccfe2857392bc81d433e39
RonaldC <ronald.concepcion@ukg.com>
2022-07-21 12:15:13 -0400

    Fixed powers and relics not triggering when a card exhausts for the remote player

--- src/main/java/stwf/cards/CardGroupPatch.java
+++ src/main/java/stwf/cards/CardGroupPatch.java
@@ -5,6 +5,7 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
 import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
 import stwf.cards.AbstractCardPatch.AbstractCardFields;
@@ -14,12 +15,36 @@ import stwf.multiplayer.Player;
 
 public class CardGroupPatch
 {
+    private static AbstractPlayer tempPlayer;
+
     @SpirePatch2(clz = CardGroup.class, method = "moveToExhaustPile")
     public static class MoveToExhaustPilePatch
     {
-        @SpireInsertPatch
-        public static SpireReturn<Void> Prefix(AbstractCard c)
+        @SpireInsertPatch()
+        public static void Prefix(AbstractCard c)
         {
+            Player cardPlayer = AbstractCardFields.playerData.get(c);
+
+            if (MultiplayerManager.inMultiplayerLobby() && cardPlayer != null)
+            {
+                tempPlayer = AbstractDungeon.player;
+                AbstractDungeon.player = cardPlayer.character;
+            }
+        }
+    }
+
+    @SpirePatch2(clz = CardGroup.class, method = "moveToExhaustPile")
+    public static class MoveToExhaustPilePatch2
+    {
+        @SpireInsertPatch(loc = 947)
+        public static SpireReturn<Void> Insert(AbstractCard c)
+        {
+            if (tempPlayer != null)
+            {
+                AbstractDungeon.player = tempPlayer;
+                tempPlayer = null;
+            }
+
             Player cardPlayer = AbstractCardFields.playerData.get(c);
             Player characterPlayer = AbstractPlayerFields.playerData.get(AbstractDungeon.player);
 
