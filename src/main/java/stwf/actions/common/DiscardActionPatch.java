e8c0af2dfbb4984649466db216df523d686ceb9d
RonaldC <ronald.concepcion@ukg.com>
2022-07-23 13:09:53 -0400

    Fixed discard card screen showing up for remote player

---
+++ src/main/java/stwf/actions/common/DiscardActionPatch.java
@@ -0,0 +1,27 @@
+package stwf.actions.common;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
+import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+
+public class DiscardActionPatch
+{
+    @SpirePatch2(clz = DrawCardAction.class, method = "update")
+    public static class GetNextActionPatch
+    {
+        @SpireInsertPatch()
+        public static SpireReturn<Void> Prefix(DrawCardAction __instance, AbstractCreature ___target)
+        {
+            if (___target == AbstractDungeon.player)
+            {
+                return SpireReturn.Continue();
+            }
+
+            __instance.isDone = true;
+            return SpireReturn.Return();
+        }
+    }
+}
